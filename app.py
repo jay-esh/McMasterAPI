4bf6ea6e5d77a71322670c2c9effee50650dda1d
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2022-10-31 17:44:58 -0400

    ggs for now

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import json
-from flask import Flask, request, session
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+# from flask_swagger_ui import get_swaggerui_blueprint
+from flask_restful import Resource, Api, reqparse
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,10 +12,12 @@ app.config['SECRET_KEY'] = "HACKTOBER"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+api = Api(app)
+
 db = SQLAlchemy(app)
 db.init_app(app)
 
-cors  = CORS(app, resources={r"/api/*": {"origin": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origin": "*"}})
 
 class Courses(db.Model):
     courseId = db.Column(db.String(150), primary_key=True)
@@ -20,21 +25,23 @@ class Courses(db.Model):
     units = db.Column(db.String(150), nullable=False)
     description = db.Column(db.String(2000), nullable=False)
 
+class Home(Resource):
+    def get(self):
+        return {"about" : "Its a cool api"}
 
-@app.route('/',methods = ['GET'])
-def home():
-    return "<h1>Server running...ig, idk</h1>"
+class CourseDetails(Resource):
+    def get(self, id):
+        course = db.get_or_404(Courses, id)
+        ans = {
+            "courseFullName": course.courseFullName,
+            "units": course.units,
+            "description": course.description
+        }
+        return jsonify(ans)
 
-@app.route('/api/<course_id>',methods = ['GET'])
-def get_course_details(course_id):
-    print(course_id)
-    course = db.get_or_404(Courses, course_id)
-    ans = {
-        "courseFullName": course.courseFullName,
-        "units" : course.units,
-        "description" : course.description
-    }
-    return ans
+api.add_resource(Home, '/')
+api.add_resource(CourseDetails, '/api/<string:id>')
+
 
 if __name__ == "__main__":
     app.run()
